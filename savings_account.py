0d2b9bfae0e9faea1f20bedfb07d8f7f2ce2ff07
Hussain Ahmad <hussainahmad.dev.17@gmail.com>
2026-01-24 13:25:55 +0500

    fixing methods

--- savings_account.py
+++ savings_account.py
@@ -1,15 +1,20 @@
 from account import Account
 from dataclasses import dataclass
 
+
+
+# Define a SavingsAccount class that inherits from Account
 @dataclass
 class SavingsAccount(Account):
     interest_rate: float = 0.02
 
+# Implement the withdraw method (polymorphism)
     def withdraw(self, amount: float):
         if amount > self._balance:
             raise ValueError("Insufficient funds")
         self._balance -= amount
-
+        
+# Implement a method to add interest to the balance
     def add_interest(self):
         self._balance += self._balance * self.interest_rate
        
