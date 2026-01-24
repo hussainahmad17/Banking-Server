6c75b4d2cbea58ef04486ca1309a340eaa006709
Hussain Ahmad <hussainahmad.dev.17@gmail.com>
2026-01-24 13:00:31 +0500

    fixing interest rates

--- savings_account.py
+++ savings_account.py
@@ -5,11 +5,11 @@ from dataclasses import dataclass
 class SavingsAccount(Account):
     interest_rate: float = 0.02
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float):
         if amount > self._balance:
             raise ValueError("Insufficient funds")
         self._balance -= amount
 
     def add_interest(self):
-        interest = self._balance * self.interest_rate
-        self._balance += interest
+        self._balance += self._balance * self.interest_rate
+       
